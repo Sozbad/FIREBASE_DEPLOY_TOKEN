1c08e1197c659adf87f2733214535c34b10fd835
Sozbad <chris.p.kelly00@gmail.com>
2025-04-13 23:45:41 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Project root
-  publicDir: 'public',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html')
-    }
+    outDir: 'dist'
   }
 });
