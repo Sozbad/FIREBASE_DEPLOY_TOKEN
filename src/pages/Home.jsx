b1b7070ba711e0026ebfd857ad25b4ebae2cc13e
Sozbad <chris.p.kelly00@gmail.com>
2025-04-14 12:57:39 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,11 @@ import ProductSearch from '../components/ProductSearch';
 
 export default function Home() {
   return (
-    <div className="p-4 pt-6 pb-16">
-      <h1 className="text-xl font-semibold mb-4">Search Products</h1>
+    <div className="min-h-screen pb-28 bg-white text-gray-900">
+      <header className="px-4 pt-6 pb-3">
+        <h1 className="text-2xl font-bold">Search</h1>
+      </header>
+
       <ProductSearch />
     </div>
   );
