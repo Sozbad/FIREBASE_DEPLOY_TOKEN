b0a6964a40c4e3b423c298e6c6aaa1b56009d027
Sozbad <chris.p.kelly00@gmail.com>
2025-04-14 09:37:41 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
+  projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_PROJECT.appspot.com",
   messagingSenderId: "YOUR_SENDER_ID",
   appId: "YOUR_APP_ID"
