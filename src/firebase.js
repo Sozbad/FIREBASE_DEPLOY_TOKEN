0d6b7bfeee16e89ac841c0bd4adea88eaaddbb89
Sozbad <chris.p.kelly00@gmail.com>
2025-04-14 09:45:41 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
-// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCozyzidnPbU6w07k1U9jc69w9Z0onSZvc",
+  authDomain: "ecorank-15295.firebaseapp.com",
+  projectId: "ecorank-15295",
+  storageBucket: "ecorank-15295.appspot.com",
+  messagingSenderId: "11032290866138",
+  appId: "1:11032290866138:web:e3bb56ec061c58635a26e3"
 };
 
 const app = initializeApp(firebaseConfig);
