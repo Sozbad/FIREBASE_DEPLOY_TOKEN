73dffddd8d9497235709639a3fb0e1406dd0508e
Sozbad <chris.p.kelly00@gmail.com>
2025-04-24 21:06:18 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ProductCard from "@/components/ProductCard";
-import ScoreBreakdownPie from "@/components/ScoreBreakdownPie";
+import ProductCard from "/components/ProductCard";
+import ScoreBreakdownPie from "/components/ScoreBreakdownPie";
 
 const HomePage = () => {
   return (
